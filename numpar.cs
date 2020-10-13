0d89d3d880c9b6931893be37429b4480bb2a7af6
paulorivasces <72433689+paulorivasces@users.noreply.github.com>
2020-10-13 23:36:47 +0200

    Números pares

---
+++ numpar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NumPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2;
+            Console.WriteLine("Ingrese un número Entero: ");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese un número Entero: ");
+            numero2 = int.Parse(Console.ReadLine());
+            if(numero1 % 2 == 0)
+            {
+                Console.WriteLine("El " + numero1 + " es par ");
+            }
+            else
+            {
+                Console.WriteLine("El " + numero1 + " no es par ");
+            }
+            if (numero2 % 2 == 0)
+            {
+                Console.WriteLine("El " + numero2 + " es par ");
+            }
+            else
+            {
+                Console.WriteLine("El " + numero2 + " no es par ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
