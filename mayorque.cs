91a95e680516cc55b03ff172f0af6ac66b51c1ba
paulorivasces <72433689+paulorivasces@users.noreply.github.com>
2020-10-13 23:35:46 +0200

    Que numero es mayor

---
+++ mayorque.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumMayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2;
+            Console.WriteLine("Ingrese un número Entero: ");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese un número Entero: ");
+            numero2 = int.Parse(Console.ReadLine());
+            if (numero1 > numero2)
+            {
+                Console.WriteLine("El numero mayor es: " + numero1);
+            }
+            else
+            {
+                Console.WriteLine("El numero mayor es: " + numero2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
