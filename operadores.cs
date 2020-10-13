e0ef2372734a975747f5edbdb1e27a5283f58913
paulorivasces <72433689+paulorivasces@users.noreply.github.com>
2020-10-13 23:34:44 +0200

    Suma, Resta, Multiplicación y Modulo de dos números enteros

---
+++ operadores.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 3;
+            int numero2 = 4;
+            int resultadosuma;
+            int resultadoresta;
+            double resultadomulti;
+            double resultadomodulo;
+
+            resultadosuma = numero1 + numero2;
+            resultadoresta = numero1 - numero2;
+            resultadomulti = numero1 * numero2;
+            resultadomodulo = numero1 % numero2;
+
+            Console.WriteLine("La suma de los numeros es: " + resultadosuma);
+            Console.WriteLine("La resta de los numeros es: " + resultadoresta);
+            Console.WriteLine("La multiplicacion de los numeros es: " + resultadomulti);
+            Console.WriteLine("El resto de los numeros es: " + resultadomodulo);
+
+            Console.ReadLine();
+        }
+    }
+}
